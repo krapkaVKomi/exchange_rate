1834510df74d0eb9d8812d9782802a9d1adabf56
User <lg2031832@gmail.com>
2023-01-30 20:04:00 +0200

    add Model and flask_migrate

---
+++ app.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/exchange_rate'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+api = Api()
+
+
+class ExchangeRate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    currency = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.date)
+    rate_to_usd = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return '<ExchangeRate %r>' % self.id
+
+
+class Main(Resource):
+    def get(self):
+        return {"info": "Some info", "nun": 56}
+
+
+api.add_resource(Main, "/api/main")
+api.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000, host="127.0.0.1")
+
